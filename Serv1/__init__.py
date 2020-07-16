764e8606b3638196e04448c131f5bc6aad51a27a
Ubuntu <AzureUser@LinuxVM.2nh0rrbjdz4ebjdzbpzmntt5oe.zx.internal.cloudapp.net>
2020-07-16 15:22:52 +0000

    2nd attempt

--- Serv1/__init__.py
+++ Serv1/__init__.py
@@ -1,15 +1,17 @@
 import logging
 
 import azure.functions as func
-import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    letterstring = requests.get('http://localhost:7071/api/Serv2')
-    nostring = requests.get('http://localhost:7071/api/Serv3')
-    output = ""
-    for i in range(5):
-        output = output + letterstring[i-1]
-        output = output + nostring[i-1]
-    return func.HttpResponse(f"{output}")
+    letterstring = req.route_params.get('http://localhost:7071/api/Serv2')
+    nostring = req.route_params.get('http://localhost:7071/api/Serv3')
+    counter = 0
+    output = ''
+    while counter < len(letterstring):
+        output = output + letterstring[counter]
+        output = output + nostring[counter]
+        counter += 1
+    message = f"{output}"
+    return func.HttpResponse(message)
--- Serv3/__init__.py
+++ Serv3/__init__.py
@@ -4,5 +4,5 @@ import random
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    nostring = ''.join(random.randint(0,10)
+    nostring = ''.join(random.randint(0,10) for i in range(5))
     return func.HttpResponse(f"{nostring}")
